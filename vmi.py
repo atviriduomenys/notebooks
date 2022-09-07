d3f1fb7abcba4a713ac33d4b8d3628d5823d2236
Evelina Zigmantaitė <evelina.zigmantaite@outlook.com>
2022-09-07 09:19:13 +0300

    Pataisyta VMI gyventojų patirtų išlaidų analizė

--- vmi.py
+++ vmi.py
@@ -17,264 +17,171 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import seaborn as sns
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+# %matplotlib inline
+
+# %% [markdown] tags=[]
+# # IŠLAIDOS
+
+# %% [markdown]
+# Agreguoti duomenys iš juridinių asmenų pateiktos informacijos apie gyventojų patirtas įmokas ir gyventojų deklaruotas išlaidas,
+# kuriomis gali būti mažinamos gyventojų apmokestinamosios pajamos. 
+# https://data.gov.lt/dataset/gyventoju-patirtos-islaidos
 
 # %%
-islaidos=pd.read_csv("Gyventojuislaidos.csv")
+islaidos=pd.read_csv("data/datasets/gov/vmi/gyventojų_patirtos_išlaidos/GyventojuIslaidos.csv")
+
+# %% [markdown] tags=[]
+# # KLAUSIMAI
 
 # %%
-#Išlaidos#
+# Kodėl daug nulinių reikšmių
+# Pataisyti brandos lygiai (visi)
+# Kodėl yra maksimali riba 600000
+
+# %% [markdown] tags=[]
+# # Duomenų apžvalga
 
 # %%
-islaidos.info() #paklausti del strukturoje esanciu beveik visu string
+pd.options.display.max_colwidth = 500
+islaidos.head(5)
+
+# %% [markdown]
+# Kai kurie Dtype neatitinka tipo nurodyto struktūros apraše. Pvz: sav_kodas, 
+
+# %%
+islaidos.info() 
 
 # %%
 islaidos.nunique()
 
-# %%
-islaidos.head(10)
+# %% [markdown] tags=[]
+# # Išlaidų rūšys pagal populiarumą
 
-# %%
-islaidos['suma'].nlargest(n=10) #10 didziausiu islaidu
-#prideti ir rusi/savivaldybe
-#is apraso "Išlaidų suma esanti virš 600000 eurų yra fiksuojama 600000 verte"
-
-# %%
-islaidos['suma'].nsmallest(n=10) #KAIP IMTI BE NULIU IR PRIDETI RUSI ARBA ISTRINTI NULINES
-
-# %%
-#------------RUSYS----------------#
-
-# %%
-islaidos_rusys=islaidos.groupby(['ir_kodas', 'pavadinimas']).sum(['suma']).drop(['metai','sav_kodas'],axis=1)
-islaidos_rusys #pagal islaidu rusies kodus susumuota islaidu sumos
-
-# %%
-mpl.rc('figure', figsize=(10,10))
-islaidos_rusys.reset_index().plot(x='ir_kodas', y='suma', c = 'red', marker = 'o',  markersize=7, grid = True).legend(labels = ['Suma'])
-plt.xlabel("Išlaidų rūšies kodas")
-plt.ylabel("Išlaidų suma per 2020 m.")
-
-# %%
-islaidos_dazniai=islaidos.value_counts(['ir_kodas','pavadinimas']).sort_index()
-islaidos_dazniai
-#suskaiciuota kiek kiekvienos rusies (pagal koda ir pavadinima) yra islaidu duomenyse
+# %% [markdown]
+# Brėžiame diagramą, kuri vaizduoja kiekvienos išlaidų rūšies dažnį šiuose duomenyse.
 
 # %%
 mpl.rc('figure', figsize=(20,10))
-islaidos_dazniai.reset_index().plot(x='ir_kodas', y=0, c = 'red', marker = 'o',  markersize=7, grid = True).legend(labels = ['Dažniai'])
-plt.xlabel("Išlaidų rūšies kodas")
-plt.ylabel("Dažniai")
+islaidos_dazniai=islaidos.value_counts(['ir_kodas','pavadinimas']).sort_values(ascending = False).to_frame()
+islaidos_dazniai.plot.barh().legend(labels=['Dažnis'])
+
+# %% [markdown]
+# Diagramos matome, kad tarp išlaidų rūšių, kuriomis gali būti mažinamos gyventojų apmokestinamosios pajamos, akivaizdžiai pirmauja gyvybės draudimo įmokos.
+
+# %% [markdown]
+# Taip pat galime pažiūrėt, kaip bendrai yra pasiskirstę visos išlaidų rūšys.
 
 # %%
-islaidos_vidurkiai=islaidos.groupby(['ir_kodas','pavadinimas']).mean().round(2).sort_values(by = 'ir_kodas').drop(['metai','sav_kodas'], axis=1)
-islaidos_vidurkiai
+vienas = islaidos.set_index(['_id','pavadinimas'])['suma'].unstack() #.plot.box(logy = True)
+fig, ax= plt.subplots(figsize=(20, 10))
+ax.set_xscale('log')
+g = sns.boxplot(data=vienas , palette='rainbow', orient="h", ax=ax)
+plt.tight_layout()
+
+# %% [markdown]
+# Iš boxplot diagramų matome, kad "Gyvybės draudimo įmokos" turi labai daug išsiskiriančių reikšmių. Kyla klausimas dėl 60000 ribos duomenyse. Taip pat matome, kad didžiausią medianą turi "Pastatų, statinių remonto, apdailos (išskyrus renovaciją) darbų išlaidos, o labiausiai svyruojančios išlaidų sumos yra dėl "Būsto kredito, suteikto iki 2008-12-31, palūkanos" ir "Nepilnamečių (iki 18 metų) vaikų (įvaikių, globotinių, kuriems nustatyta nuolatinė globa šeimoje) priežiūros paslaugų išlaidos."
 
 # %%
-mpl.rc('figure', figsize=(10,10))
-islaidos_vidurkiai.reset_index().plot(x='ir_kodas', y='suma', c = 'red', marker = 'o',  markersize=7, grid = True).legend(labels = ['Vidurkiai'])
-plt.xlabel("Išlaidų rūšies kodas")
-plt.ylabel("2020 m. vidurkis")
+rūšis_islaidos = islaidos.loc[islaidos['pavadinimas'] == 'Gyvybės draudimo įmokos.']
+count_row_visos = islaidos.shape[0] 
+count_row_gd = rūšis_islaidos.shape[0]
+def percentage(part, whole):
+ Percentage = 100 * float(part)/float(whole)
+ return str(Percentage)
+percentage(count_row_gd, count_row_visos)
 
-# %%
-mpl.rc('figure', figsize=(20,10))
-islaidos_dv = [islaidos_dazniai,islaidos_vidurkiai]
-result=pd.concat(islaidos_dv)
-result.hist() #0 pakeisti i daznius
+# %% [markdown]
+# Skaičiuojant procentinę dalį pastebime, kad "Gyvybės draudimo įmokos" sudaro net 62 % visų išlaidų rūšių, kuriomis gali būti mažinamos gyventojų apmokestinamosios pajamos
 
-# %%
-islaidos_dazniai.max()
-
-# %%
-#---------------SAVIVALDYBES------------------#
-
-# %%
-islaidos_sav=islaidos.groupby(['sav_kodas', 'sav_pavadinimas']).sum(['suma']).sort_values(by ='sav_kodas').drop(['metai','ir_kodas'],axis=1)#kiek kiekvienam rusies savivaldybes kodui priklauso islaidu
-islaidos_sav['suma'].nlargest(n=10)
-
-# %%
-islaidos_sav['suma'].nsmallest(n=10).sort_values(ascending = False)
-
-# %%
-mpl.rc('figure', figsize=(20,10))
-islaidos_sav.reset_index().plot(x='sav_kodas', y='suma', c = 'blue', marker = 'o',  markersize=3, grid = True).legend(labels = ['Išlaidos'])
-plt.xlabel("Savivaldybės kodas")
-plt.ylabel("Išlaidų savivaldybėse suma (2020 m.)")
-
-# %%
-dazniai_sav = islaidos.value_counts(['sav_kodas','sav_pavadinimas']).sort_index()
-dazniai_sav.head(5)
+# %% [markdown]
+# Brėžiame diagramą, kuri vaizduoja "Gyvybės draudimo įmokos" sumas ir jų dažnius.
 
 # %%
 mpl.rc('figure', figsize=(20,10))
-dazniai_sav.reset_index().plot(x='sav_kodas', y=0, c = 'red', marker = 'o',  markersize=7, grid = True).legend(labels = ['Dažniai'])
-plt.xlabel("Savivaldybės kodas")
-plt.ylabel("Dažniai")
+rūšis_islaidos['suma'].plot.hist(bins = 100, logy = True, color='y', grid = True)
+
+# %% [markdown]
+# Pastebime aiškiai išsiskiriančias reikšmes - pavieniai atvejai, kuomet "Gyvybės draudimo įmokos" sumos siekia 100000 ar net 600000.
 
 # %%
-vidurkiai_sav=islaidos.groupby(['sav_kodas','sav_pavadinimas']).mean().round(2).drop(['metai', 'ir_kodas'], axis=1).sort_index()
-vidurkiai_sav.head(10)
+islaidos_gyv_draud = islaidos.loc[islaidos['pavadinimas'] == 'Gyvybės draudimo įmokos.']
+count_row = islaidos_gyv_draud.shape[0]
+isl = islaidos_gyv_draud['suma'].nlargest(2732).to_frame()
+m_isl = islaidos_gyv_draud['suma'].nsmallest(270442).to_frame()
+count_row_visos = islaidos_gyv_draud.shape[0] 
+count_row_gd = isl.shape[0]
+def percentage(part, whole):
+ Percentage = 100 * float(part)/float(whole)
+ return str(Percentage)
+percentage(count_row_gd, count_row_visos)
+
+# %% [markdown]
+# Galime patikrinti pavyzdžiui kokią dalį iš visos "Gyvybės draudimo įmokos" išlaidų sumos sudaro 1 % daugiausiai sumokančių žmonių.
 
 # %%
-mpl.rc('figure', figsize=(20,10))
-vidurkiai_sav.reset_index().plot(x='sav_kodas', y='suma', c = 'red', marker = 'o',  markersize=7, grid = True).legend(labels = ['Vidurkiai'])
-plt.xlabel("Savivaldybės kodas")
-plt.ylabel("Išlaidų savivaldybėse vidurkiai (2020 m.)")
+islaidos_all = islaidos_gyv_draud['suma'].sum()
+islaidos_isl = isl['suma'].sum()
+islaidos_m_isl = m_isl['suma'].sum()
+count_row_visos = islaidos_all 
+count_row_gd = islaidos_isl
+def percentage(part, whole):
+ Percentage = 100 * float(part)/float(whole)
+ return str(Percentage)
+percentage(count_row_gd, count_row_visos)
+
+# %% [markdown]
+# Matome, kad 1 % didžiausias įmokas sumokančių mokesčių mokėtojų išleidžiamos sumos gyvybės draudimui sudaro beveik 9 % visos šios rūšies išlaidų sumos.
+
+# %% [markdown]
+# # Vilniaus miesto ir Neringos savivaldybių išlaidų palyginimas
+
+# %% [markdown]
+# Norėdami palyginti savivaldybes tarpusavyje, pirmiausia turime jas normalizuoti pagal gyventojų skaičių. Tam nusiskaitome duomenis iš gyventojų registro apie Lietuvos gyventojų amžių ir lytį pagal savivaldybes
 
 # %%
-islaidos.loc[islaidos['sav_kodas'] == 23]
+rc = pd.read_csv("data/datasets/gov/rc/gyventojų_registro_duomenys/01_gr_open_amzius_lytis_pilietybes_sav_r1.csv")
+
+# %% [markdown]
+# Gyventojų registro (GR) duomenys apie Lietuvos Respublikos teritorijoje įregistruotus fizinius asmenis. GR duomenys apie Lietuvos gyventojų amžių ir lytį pagal savivaldybes. Duomenų geografinė imtis — visa šalies teritorija. Brandos lygis — III. Atnaujinimo dažnumas — kas 12 mėn. Duomenų formatas — CSV. Licencija — CC BY 4.0. Atvėrimo data — 2022-01-14
+# https://www.registrucentras.lt/p/1539
+# https://data.gov.lt/dataset/gyventoju-registro-duomenys-apie-lietuvos-gyventoju-amziu-ir-lyti-pagal-savivaldybes
 
 # %%
-islaidos_miestui = islaidos.value_counts(['sav_pavadinimas', 'ir_kodas','pavadinimas']).sort_index()
-islaidos_miestui.head()#rasti kiekveinai savivaldybei sriti, kur dazniausios/daugiausiai islaidu
-islaidos_miestui['Neringos sav.'].nlargest(n=3) #trys popiliariausios tam mieste islaidu rusys)
+# islaidos_sav=islaidos.groupby(['sav_kodas', 'sav_pavadinimas'])['suma'].sum().sort_values(ascending = False)
+# islaidos_sav.head(10)
 
 # %%
-islaidos['sav_pavadinimas'].unique()
+rc.head()
 
 # %%
-islaidos_rusiai = islaidos.value_counts(['pavadinimas','sav_pavadinimas']).sort_index()
-islaidos_rusiai.head()#rasti kiekveinai savivaldybei sriti, kur dazniausios/daugiausiai islaidu
-islaidos_rusiai['Nepilnamečių (iki 18 metų) vaikų (įvaikių, globotinių, kuriems nustatyta nuolatinė globa šeimoje) priežiūros paslaugų išlaidos.'].nlargest(n=3)
+rc.nunique()
+
+# %% [markdown]
+# Pasirenkame palyginti būtent Neringos ir Vilniaus miesto savivaldybes, nes apskaičiavus visų išlaidų sumos medianą, paaiškėja, jog Neringos savivaldybėje išlaidų dydžiai yra aukščiausi. 
 
 # %%
-islaidos['pavadinimas'].unique()
+mediana = islaidos.groupby(['sav_pavadinimas'])['suma'].median().sort_values(ascending = False)
+mediana.head().to_frame()
+
+# %% [markdown]
+# Todėl lyginame jas dalindami visų išlaidų rūšių bendras sumas iš toje savivaldybėje gyvenančių gyventojų skaičiaus, šiuos duomenis gauname iš gyventojų registro. 
 
 # %%
-islaidos['suma'].plot.box(logy = True)
+vilnius = islaidos.loc[islaidos['sav_pavadinimas'] == 'Vilniaus m. sav.']
+vilnius1 = rc.loc[rc['sav_pavadinimas'] == 'Vilniaus m. sav.']
+is_viso_vilnius = vilnius1.shape[0]
+vilnius_dal_2 = vilnius.groupby(['pavadinimas'])['suma'].sum()/(is_viso_vilnius)
+vilnius_dal_2.to_frame()
 
 # %%
-islaidos_sav.plot.box(logy = True)
+neringa = islaidos.loc[islaidos['sav_pavadinimas'] == 'Neringos sav.']
+neringa1 = rc.loc[rc['sav_pavadinimas'] == 'Neringos sav.']
+is_viso_neringa = neringa1.shape[0]
+neringa_dal_2 = neringa.groupby(['pavadinimas'])['suma'].sum()/(is_viso_neringa)
+neringa_dal_2.to_frame()
 
-# %%
-sns.histplot(data=islaidos[islaidos['suma'] < 7000], x = 'suma', kde = True)
-
-# %%
-sns.histplot(data=islaidos[islaidos['suma'] < 2000], x = 'suma')
-
-# %%
-#######################################################################################################
-
-# %%
-#######################################################################################################
-
-# %%
-#Pajamos#
-
-# %%
-pajamos=pd.read_csv("GyventojuPajamos.csv")
-
-# %%
-pajamos.info() #kodėl nėra non-null ir count stulpeliu
-
-# %%
-pajamos.nunique()
-
-# %%
-pajamos.head(10)
-
-# %%
-#RUSYS#
-
-# %%
-pajamos_rusys=pajamos.groupby(['kodas', 'aprasas']).sum(['suma']).drop(['eil_num','sav_kodas', 'metai'],axis=1)
-pajamos_rusys #pagal islaidu rusies kodus ir aprasus susumuota islaidu sumos
-
-# %%
-plt.rcParams['figure.figsize']=[20,10]
-pajamos_rusys.reset_index().plot(x='kodas', y = 'suma', c = 'blue', marker = 'o',  markersize=3, grid = True).legend(labels = ['Suma'])
-plt.xlabel("Pajamų kilmės kodas")
-plt.ylabel("Pajamų suma")
-
-# %%
-pajamos_dazniai=pajamos.value_counts(['kodas', 'aprasas']).sort_index()
-pajamos_dazniai
-#cia suskaiciuota kiek kiekvienos rusies (pagal koda ir pavadinima) yra islaidu duomenyse
-
-# %%
-mpl.rc('figure', figsize=(20,10))
-pajamos_dazniai.reset_index().plot(x='kodas', y = 0, c = 'blue', marker = 'o',  markersize=3, grid = True).legend(labels = ['Dažniai'])
-plt.xlabel("Pajamų kilmės kodas")
-plt.ylabel("Dažniai")
-
-# %%
-pajamos_vidurkiai=pajamos.groupby(['aprasas','kodas']).mean().round(2).sort_values(by = 'kodas').drop(['metai','sav_kodas','eil_num'], axis=1)
-pajamos_vidurkiai 
-
-# %%
-mpl.rc('figure', figsize=(20,10))
-pajamos_vidurkiai.reset_index().plot(x='kodas', y='suma', c = 'red', marker = 'o',  markersize=7, grid = True).legend(labels = ['Vidurkiai'])
-plt.xlabel("Pajamų rūšies kodas")
-plt.ylabel("2020 m. vidurkis")
-
-# %%
-pajamos_dv = [pajamos_dazniai,pajamos_vidurkiai]
-result=pd.concat(pajamos_dv)
-result.hist()
-
-# %%
-#SAVIVALDYBES#
-
-# %%
-pajamos_sav=pajamos.groupby(['sav_kodas', 'sav_pavadinimas']).sum(['suma']).sort_values(by ='sav_kodas').drop(['metai','kodas','eil_num'],axis=1)
-pajamos_sav.head(10) #kiek kiekvienam rusies savivaldybes kodui priklauso islaidu
-
-# %%
-plt.rcParams['figure.figsize']=[20,10]
-pajamos_sav.reset_index().plot(x = 'sav_kodas', y = 'suma', c = 'blue', marker = 'o',  markersize=3, grid = True).legend(labels = ['Suma'])
-plt.xlabel("Savivaldybės kodas")
-plt.ylabel("Savivaldybės pajamų suma (2020 m.)")
-#kaip paimti atskirus daugiausiai (ar mažiausiai) išleidžiančius miestus, padidinti tikslumą diagramoje
-
-# %%
-dazniai_sav_paj=pajamos.value_counts(['sav_kodas','sav_pavadinimas']).sort_index()
-dazniai_sav_paj.head(10)
-
-# %%
-mpl.rc('figure', figsize=(20,10))
-dazniai_sav_paj.reset_index().plot(x='sav_kodas', y = 0, c = 'blue', marker = 'o',  markersize=3, grid = True).legend(labels = ['Dažniai'])
-plt.xlabel("Savivaldybės kodas")
-plt.ylabel("Dažniai")
-
-# %%
-vidurkiai_sav_paj=pajamos.groupby(['sav_kodas','sav_pavadinimas']).mean().round(2).drop(['metai', 'kodas', 'eil_num'], axis=1).sort_index()
-vidurkiai_sav_paj.head(10)
-
-# %%
-mpl.rc('figure', figsize=(20,10))
-vidurkiai_sav_paj.reset_index().plot(x='sav_kodas', y='suma', c = 'red', marker = 'o',  markersize=7, grid = True).legend(labels = ['Vidurkiai'])
-plt.xlabel("Savivaldybės kodas")
-plt.ylabel("Pajamų savivaldybėse vidurkiai (2020 m.)")
-
-# %%
-pajamos.loc[pajamos['sav_kodas'] == 23]
-
-# %%
-miestui_paj = pajamos.value_counts(['sav_pavadinimas', 'kodas','aprasas']).sort_index()
-miestui_paj.head()#rasti kiekveinai savivaldybei sriti, kur dazniausios/daugiausiai islaidu
-miestui_paj['Alytaus m. sav.'].nlargest(n=3) #trys popiliariausios tam mieste islaidu rusys) ir taip su visais kazka bendro
-
-# %%
-pajamos['sav_pavadinimas'].unique()
-
-# %%
-rusiai_paj = pajamos.value_counts(['aprasas','sav_pavadinimas']).sort_index()
-rusiai_paj.head()#rasti kiekveinai savivaldybei sriti, kur dazniausios/daugiausiai islaidu
-rusiai_paj['Azartinių lošimų laimėjimų pajamos'].nlargest(n=3)
-
-# %%
-pajamos['aprasas'].unique()
-
-# %%
-pajamos['suma'].plot.box(logy = True)
-
-# %%
-pajamu_sav['suma'].plot.box(logy = True)
-
-# %%
-sns.histplot(data=pajamos[pajamos['suma'] <20000], x = 'suma')
-
-# %%
-sns.histplot(data=pajamos[pajamos['suma'] <1000], x = 'suma')
-
-# %%
-pajamos['suma'].min() #kodel and dia kitaip
+# %% [markdown]
+# Peržiūrėjus abi lenteles galime daryti išvadą, jog Neringos savivaldybėje gyvena turtingesni žmonės ir, kad ten pragyvenimas greičiausiai kainuoja daugiau, nei Vilniaus miesto savivaldybėje, kadangi beveik visos apskaičiuotos sumos Neringos savivaldybėje lenkia Vilniaus miesto savivaldybės išlaidas
